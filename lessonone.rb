eb2cb3cef35aa841d37faaacda0877130978b3f6
Cezana Roussel <44489585+cezanaroussel@users.noreply.github.com>
2019-06-04 03:40:52 -0700

    Add files via upload

---
+++ lessonone.rb
@@ -0,0 +1,33 @@
+
+
+# use alt + r to run your code
+# puts = prints out whatever you specify onto a new line
+# print = everything printed out on the same line
+puts "     /|"
+puts "    / |"
+puts "   /  |"
+puts "  /___|"
+
+#example of how print puts strings on same line
+
+
+#when dealing with ruby you'll be dealing with a lot of data
+#look at the name/age, this is where variables come in
+#we want to track and maintain them
+#we can put name and age in a container
+#the container is called a variable
+#give it a descriptive name and give it a _
+character_name = "John"
+character_age = "35"
+puts ("There once was a man named " + character_name)
+puts ("he was " + character_age +" years old.")
+
+#you will notice the name changes halfway through the story
+#due to the ability to change the variable
+
+character_name = "Thomas"
+puts ("He really liked the name " + character_name)
+puts ("but didn't like being " + character_age)
+
+# we can represent / store different types of data inside of our variables
+# next lesson, data types
---
+++ lessontwo.rb
@@ -0,0 +1,25 @@
+# data types are diff types of data we can use in ruby
+# you can store data in variables
+# 1st type! a string is basically just plain text
+# ex of a variable with a string value inside it below
+name = "Brent"
+age = 30
+# 30 is an integer, a whole number
+# a decimal is different, referred to as floating point numbers
+gpa = 3.9
+
+# 2nd type! a boolean is a t/f data type
+isfemale = true
+istall = false
+
+ # setting a variable to 'nil' means it has no value
+ flaws = nil
+
+#how to print out quotation marks within a string
+puts "\"Giraffe\" Academy"
+
+# convert to all uppercase letters
+#you can downcase too as you see below
+phrase = "Giraffe Academy"
+puts phrase.upcase()
+puts phrase.downcase()
